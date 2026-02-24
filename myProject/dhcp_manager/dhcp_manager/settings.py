9db0f8beefdc1d7a5954ac18c00fe1f7635dd8fb
ElyasShamal <Elyas.shamal2@gmail.com>
2026-02-23 23:51:43 -0500

    ready for deployment

--- myProject/dhcp_manager/dhcp_manager/settings.py
+++ myProject/dhcp_manager/dhcp_manager/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
